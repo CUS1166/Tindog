e34ea197d43f24f322d616b0993642a3eb6c5dee
Jderege <jderege@yahoo.com>
2017-04-12 15:38:54 -0400

    Please work

--- src/TinDogMain.java
+++ src/TinDogMain.java
@@ -1,13 +1,11 @@
+
 import javax.swing.JFrame;
-public class TinDogMain {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		//String dog = new String();
-		JFrame frame = new loginFrame();
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setVisible(true);
-		
+public class TinDogMain
+{
+	public static void main(String[] args)
+	{
+		String dog = new String();
+
 	}
-
 }
--- src/loginFrame.java
+++ src/loginFrame.java
@@ -2,24 +2,15 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.ButtonGroup;
 import javax.swing.JButton;
-import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JRadioButton;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
 import javax.swing.JTextField;
 
 public class loginFrame extends JFrame{
 	private static final int FRAME_WIDTH = 400;
 	private static final int FRAME_HEIGHT = 300;
-	
-	private static final int AREA_ROWS = 20;
-	private static final int AREA_COLUMNS = 40;
 	private static final int FIELD_WIDTH = 10;
 	private JLabel nameLabel;
 	private JTextField nameField;
