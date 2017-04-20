9b0b61185f6f8bcde9685cc311cbe08a7e543b38
Jderege <jderege@yahoo.com>
2017-04-20 14:55:43 -0400

    Added Filter Frame code
    Added Jewell's code with slight changes to make it work correctly and
    look good

--- src/TinDogMain.java
+++ src/TinDogMain.java
@@ -8,12 +8,12 @@ public class TinDogMain
 	{
 		String dog = new String();
 		//Does this work?
-		JFrame frame1 = new settingsFrame();
+		JFrame frame1 = new FilterFrame();
 		frame1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame1.setVisible(true);
 		
-		JFrame frame2 = new loginFrame();
+		/*JFrame frame2 = new loginFrame();
 		frame2.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame2.setVisible(true);
+		frame2.setVisible(true);*/
 	}
 }
