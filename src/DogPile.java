44d385c3da62204c6f8a85172b7aa209d82e4659
jfoga189 <jack.fogarty95@gmail.com>
2017-04-24 16:27:06 -0400

    more classes!

---
+++ src/DogPile.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+public class DogPile
+{
+	private Queue<Dog> dogPile;
+	private int age1;
+	private int age2;
+	private String breed;
+	private int gender;
+	private String shelter;
+	
+	public DogPile()
+	{
+		dogPile = new LinkedList<Dog>();
+	}
+}
