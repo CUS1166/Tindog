1ac74e2abba9a23f8960ae31f392fcffff82a530
jfoga189 <jack.fogarty95@gmail.com>
2017-04-25 20:23:42 -0400

    applying filter to dogPile

--- src/DogPile.java
+++ src/DogPile.java
@@ -9,6 +9,7 @@ public class DogPile
 	private String breed;
 	private int gender;
 	private String shelter;
+	private boolean filter;
 	
 	public DogPile()
 	{
@@ -18,10 +19,40 @@ public class DogPile
 		breed = "";
 		gender = 2;
 		shelter = "";
+		filter = false;
 	}
 	
 	public Queue<Dog> createPile()
 	{
+		for(int i = 0; i < 50; i++)
+		{
+			checkFilter();
+			
+			int dogID = (int)Math.random();
+			Dog d = new Dog();
+			
+			dogPile.add(d);
+			
+			if(filter)
+			{
+				//if any of the dog's values do not fit the filter it is removed
+			}
+			
+		}
 		return dogPile;
+	}
+	
+	public void checkFilter()
+	{
+		if(age1 != 0)
+			filter = true;
+		else if(age2 != 100)
+			filter = true;
+		else if(!breed.isEmpty())
+			filter = true;
+		else if(gender != 2)
+			filter = true;
+		else if(!shelter.isEmpty())
+			filter = true;
 	}
 }
