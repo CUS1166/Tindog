49ede9234e7c5bc72e448dd6d735e6814887b979
Nick Skrotzki <lexicdark5529@gmail.com>
2017-04-29 15:07:38 -0400

    Library changes

--- src/TinDogMain.java
+++ src/TinDogMain.java
@@ -9,7 +9,7 @@ import java.sql.*;
 public class TinDogMain
 {
 	private static ActionListener nuca_listener;
-	//private ActionListener login_listener;
+	private ActionListener login_listener;
 	public static void main(String[] args)
 	{
 
